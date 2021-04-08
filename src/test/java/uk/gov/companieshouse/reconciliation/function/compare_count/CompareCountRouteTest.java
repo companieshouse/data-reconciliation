c8164cca24f99fd4c149d2e47c6f3a92ca8148e1
dgroves-ch <dgroves@companieshouse.gov.uk>
2021-04-08 16:19:37 +0100

    Compare number of companies in CHIPS with CHS
    * Implement a route to compare the number of corporate bodies in CHIPS
    with the number of company profiles in MongoDB.
    * The route will be triggered periodically using a cron expression and
    will initially output the result to the logs.

---
+++ src/test/java/uk/gov/companieshouse/reconciliation/function/compare_count/CompareCountRouteTest.java
@@ -0,0 +1,84 @@
+package uk.gov.companieshouse.reconciliation.function.compare_count;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.ExpressionBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@CamelSpringBootTest
+@SpringBootTest
+@DirtiesContext
+@TestPropertySource(locations = "classpath:application-stubbed.properties")
+public class CompareCountRouteTest {
+
+    @Autowired
+    private CamelContext context;
+
+    @Produce("direct:compare_count")
+    private ProducerTemplate template;
+
+    @EndpointInject("mock:a")
+    private MockEndpoint mockCorporateBodyCountEndpoint;
+
+    @EndpointInject("mock:b")
+    private MockEndpoint mockCompanyProfileCountEndpoint;
+
+    @EndpointInject("mock:result")
+    private MockEndpoint mockResult;
+
+    @AfterEach
+    void after() {
+        mockResult.reset();
+    }
+
+    @Test
+    void testOracleContainsMoreRecordsThanMongo() throws InterruptedException {
+        mockCorporateBodyCountEndpoint.returnReplyBody(ExpressionBuilder.constantExpression(BigDecimal.valueOf(15)));
+        mockCompanyProfileCountEndpoint.returnReplyBody(ExpressionBuilder.constantExpression(10L));
+        mockResult.allMessages().body().isEqualTo("A has 5 more things than B");
+        template.sendBodyAndHeaders(0, createHeaders());
+        MockEndpoint.assertIsSatisfied(context);
+    }
+
+    @Test
+    void testMongoContainsMoreRecordsThanOracle() throws InterruptedException {
+        mockCorporateBodyCountEndpoint.returnReplyBody(ExpressionBuilder.constantExpression(BigDecimal.valueOf(10)));
+        mockCompanyProfileCountEndpoint.returnReplyBody(ExpressionBuilder.constantExpression(20L));
+        mockResult.allMessages().body().isEqualTo("B has 10 more things than A");
+        template.sendBodyAndHeaders(0, createHeaders());
+        MockEndpoint.assertIsSatisfied(context);
+    }
+
+    @Test
+    void testOracleAndMongoDBContainSameNumberOfRecords() throws InterruptedException {
+        mockCorporateBodyCountEndpoint.returnReplyBody(ExpressionBuilder.constantExpression(BigDecimal.valueOf(10)));
+        mockCompanyProfileCountEndpoint.returnReplyBody(ExpressionBuilder.constantExpression(10));
+        mockResult.allMessages().body().isEqualTo("A and B contain the same number of things");
+        template.sendBodyAndHeaders(0, createHeaders());
+        MockEndpoint.assertIsSatisfied(context);
+    }
+
+    private Map<String, Object> createHeaders() {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("Src", "mock:a");
+        headers.put("SrcName", "A");
+        headers.put("Target", "mock:b");
+        headers.put("TargetName", "B");
+        headers.put("Comparison", "things");
+        headers.put("Destination", "mock:result");
+        return headers;
+    }
+}
