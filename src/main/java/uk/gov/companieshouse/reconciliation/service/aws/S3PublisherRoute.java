0f9373629c596695e885e54e8b37f797876648bd
kinpang-CH <kpang@companieshouse.gov.uk>
2021-06-16 11:07:39 +0100

    Add Javadocs to S3PublisherRoute.

--- src/main/java/uk/gov/companieshouse/reconciliation/service/aws/S3PublisherRoute.java
+++ src/main/java/uk/gov/companieshouse/reconciliation/service/aws/S3PublisherRoute.java
@@ -2,7 +2,12 @@ package uk.gov.companieshouse.reconciliation.service.aws;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.reconciliation.function.email.PublisherResourceRequest;
 
+/**
+ * Publishes {@link PublisherResourceRequest resource requests} by uploading CSV files to S3,
+ * assigning a pre-signed url for each request and finally puts the url into a ResourceLinkReference header.
+ */
 @Component
 public class S3PublisherRoute extends RouteBuilder {
 
