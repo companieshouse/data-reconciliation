d850eac51d5f2ebc25abf57fd073d26c77e9449d
kinpang-CH <kpang@companieshouse.gov.uk>
2021-06-16 11:09:20 +0100

    Fix typo

--- src/main/java/uk/gov/companieshouse/reconciliation/service/aws/S3PublisherRoute.java
+++ src/main/java/uk/gov/companieshouse/reconciliation/service/aws/S3PublisherRoute.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.reconciliation.function.email.PublisherResourceRequest;
 
 /**
- * Publishes {@link PublisherResourceRequest resource requests} by uploading CSV files to S3,
- * assigning a pre-signed url for each request and finally puts the url into a ResourceLinkReference header.
+ * Publishes {@link PublisherResourceRequest resource request} by uploading CSV files to S3.
+ * Assigns a pre-signed url for each request and finally puts the url into a ResourceLinkReference header.
  */
 @Component
 public class S3PublisherRoute extends RouteBuilder {
