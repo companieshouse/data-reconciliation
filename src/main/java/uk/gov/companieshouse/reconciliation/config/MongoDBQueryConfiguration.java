f18f1ef50770edb37b23d45fe90dc4e90b28e7ca
dgroves-ch <dgroves@companieshouse.gov.uk>
2021-06-29 10:57:57 +0100

    Add Javadocs to MongoDBConfiguration
    * MongoDBConfiguration contains aggregation pipelines used for
    disqualifications and insolvencies.

--- src/main/java/uk/gov/companieshouse/reconciliation/config/MongoDBQueryConfiguration.java
+++ src/main/java/uk/gov/companieshouse/reconciliation/config/MongoDBQueryConfiguration.java
@@ -12,14 +12,23 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Queries used to retrieve data from MongoDB.
+ */
 @Configuration
 public class MongoDBQueryConfiguration {
 
+    /**
+     * @return An aggregation pipeline used to fetch officer_id_raw fields from a collection of disqualified officers.
+     */
     @Bean
     List<Bson> disqualifiedOfficerAggregationQuery() {
         return Collections.singletonList(Aggregates.project(Projections.fields(Projections.include("officer_id_raw"))));
     }
 
+    /**
+     * @return An aggregation pipeline used to fetch insolvency case counts from a collection of insolvencies.
+     */
     @Bean
     List<Bson> insolvencyAggregationQuery() {
         return Arrays.asList(
