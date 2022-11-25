526c43cb822ed06eed4f726d4a8187e9240edca4
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2022-11-25 11:53:02 +0000

    Amended SQL queries for company count and company collection

--- src/main/resources/sql/company_collection.sql
+++ src/main/resources/sql/company_collection.sql
@@ -4,12 +4,12 @@ FROM (
     SELECT incorporation_number as item
     FROM corporate_body cb
     WHERE cb.trading_status_type_id IN (1, 2)
-        OR (cb.action_code_date >= TO_DATE('20100101', 'YYYYMMDD') AND cb.action_code_type_id >= 9000)
-    UNION
-    SELECT incorporation_number
-    FROM corporate_body cb
-    INNER JOIN transaction t ON t.corporate_body_id = cb.corporate_body_id
-    WHERE t.transaction_type_id IN (541, 542) AND transaction_status_type_id = 9 AND transaction_status_date = TO_DATE('20100105','YYYYMMDD')
+        OR substr(cb.incorporation_number,1,2)  IN  ('IP', 'SP', 'NP', 'NO', 'RS', 'R0', 'AC', 'SA', 'NA', 'IC', 'SI', 'NV', 'PC', 'SG', 'CE', 'RC', 'SR', 'NR', 'ZC', 'SZ', 'NZ' )
+        OR ( cb.action_code_type_id >= 9000 
+            AND ( SELECT coalesce(MAX(transaction_status_date) ,cb.action_code_date)
+                      FROM DISSOLUTION_TRANSACTION tr
+                      WHERE tr.corporate_body_id = cb.corporate_body_id
+                      AND tr.transaction_type_id in (541,542,566))  >= to_date('01/01/2010','dd/mm/yyyy'))
     UNION
     SELECT foreign_branch_number as item
     FROM (
