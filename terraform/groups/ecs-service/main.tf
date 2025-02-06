101c5edcaede8586b52d4d23151db4237b9d85e5
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2025-02-06 10:31:32 +0000

    increase health check grace period and formatted indentation

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -42,9 +42,11 @@ module "ecs-service" {
   
 
   # ECS Task container health check
-  use_task_container_healthcheck = true
-  healthcheck_path               = local.healthcheck_path
-  healthcheck_matcher            = local.healthcheck_matcher
+  use_task_container_healthcheck    = true
+  healthcheck_path                  = local.healthcheck_path
+  healthcheck_matcher               = local.healthcheck_matcher
+  health_check_grace_period_seconds = 240
+  healthcheck_healthy_threshold     = "2"
 
   # Docker container details
   docker_registry   = var.docker_registry
