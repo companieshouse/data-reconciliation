dbc9ba4dd53e5ed14d996854a36e4ed70afa8bae
dhawes-ch <dhawes@companieshouse.gov.uk>
2025-02-11 15:56:28 +0000

    add actuator to avoid ecs recycle? config changes

--- src/main/java/uk/gov/companieshouse/reconciliation/config/aws/S3ClientConfig.java
+++ src/main/java/uk/gov/companieshouse/reconciliation/config/aws/S3ClientConfig.java
@@ -2,8 +2,10 @@ package uk.gov.companieshouse.reconciliation.config.aws;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -11,11 +13,14 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3ClientConfig {
 
+    @Autowired
+    CamelContext camelContext;
+
     @Value("${aws.region}")
     private String awsRegion;
 
     @Bean
-    public S3Client s3Client(CamelContext camelContext) {
+    public S3Client s3Client() {
         S3Client s3Client = S3Client.builder().region(Region.of(awsRegion))
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create()).build();
         camelContext.getRegistry().bind("s3Client", s3Client);
--- src/main/java/uk/gov/companieshouse/reconciliation/service/aws/S3PublisherRoute.java
+++ src/main/java/uk/gov/companieshouse/reconciliation/service/aws/S3PublisherRoute.java
@@ -9,27 +9,19 @@ import uk.gov.companieshouse.reconciliation.common.RetryableRoute;
 import uk.gov.companieshouse.reconciliation.function.email.PublisherResourceRequest;
 
 /**
- * Publishes {@link PublisherResourceRequest resource request} by uploading CSV files to S3.
- * Assigns a pre-signed download link for each request than puts that url into a ResourceLinkReference header.
+ * Publishes {@link PublisherResourceRequest resource request} by uploading CSV files to S3. Assigns
+ * a pre-signed download link for each request than puts that url into a ResourceLinkReference
+ * header.
  */
 @Component
 public class S3PublisherRoute extends RetryableRoute {
 
-    @Autowired
-    private S3Client s3Client;
-
     @Override
     public void configure() {
         super.configure();
-        from("direct:s3-publisher")
-                .onException(AwsServiceException.class)
-                    .handled(true)
-                    .log(LoggingLevel.ERROR, "Failed to publish results to S3.")
-                    .setHeader("Failed").constant(true)
-                .end()
-                .toD("${header.Upload}")
-                .toD("${header.Presign}")
-                .setHeader("ResourceLinkReference", body())
-                .end();
+        from("direct:s3-publisher").onException(AwsServiceException.class).handled(true)
+                .log(LoggingLevel.ERROR, "Failed to publish results to S3.").setHeader("Failed")
+                .constant(true).end().toD("${header.Upload}").toD("${header.Presign}")
+                .setHeader("ResourceLinkReference", body()).end();
     }
 }
