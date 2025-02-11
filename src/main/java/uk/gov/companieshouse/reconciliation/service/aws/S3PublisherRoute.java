9d9931ae902149a83c587f821ff644ec01c7fb92
dhawes-ch <dhawes@companieshouse.gov.uk>
2025-02-11 16:03:21 +0000

    undo formatting change

--- src/main/java/uk/gov/companieshouse/reconciliation/service/aws/S3PublisherRoute.java
+++ src/main/java/uk/gov/companieshouse/reconciliation/service/aws/S3PublisherRoute.java
@@ -1,17 +1,14 @@
 package uk.gov.companieshouse.reconciliation.service.aws;
 
 import org.apache.camel.LoggingLevel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
-import software.amazon.awssdk.services.s3.S3Client;
 import uk.gov.companieshouse.reconciliation.common.RetryableRoute;
 import uk.gov.companieshouse.reconciliation.function.email.PublisherResourceRequest;
 
 /**
- * Publishes {@link PublisherResourceRequest resource request} by uploading CSV files to S3. Assigns
- * a pre-signed download link for each request than puts that url into a ResourceLinkReference
- * header.
+ * Publishes {@link PublisherResourceRequest resource request} by uploading CSV files to S3.
+ * Assigns a pre-signed download link for each request than puts that url into a ResourceLinkReference header.
  */
 @Component
 public class S3PublisherRoute extends RetryableRoute {
@@ -19,9 +16,15 @@ public class S3PublisherRoute extends RetryableRoute {
     @Override
     public void configure() {
         super.configure();
-        from("direct:s3-publisher").onException(AwsServiceException.class).handled(true)
-                .log(LoggingLevel.ERROR, "Failed to publish results to S3.").setHeader("Failed")
-                .constant(true).end().toD("${header.Upload}").toD("${header.Presign}")
-                .setHeader("ResourceLinkReference", body()).end();
+        from("direct:s3-publisher")
+                .onException(AwsServiceException.class)
+                    .handled(true)
+                    .log(LoggingLevel.ERROR, "Failed to publish results to S3.")
+                    .setHeader("Failed").constant(true)
+                .end()
+                .toD("${header.Upload}")
+                .toD("${header.Presign}")
+                .setHeader("ResourceLinkReference", body())
+                .end();
     }
 }
