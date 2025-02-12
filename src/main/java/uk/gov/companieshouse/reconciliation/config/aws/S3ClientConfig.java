13d76787cb5402c759c8eb2a9a113df2385b4041
dhawes-ch <dhawes@companieshouse.gov.uk>
2025-02-12 14:42:34 +0000

    use spring config class

--- src/main/java/uk/gov/companieshouse/reconciliation/config/aws/S3ClientConfig.java
+++ src/main/java/uk/gov/companieshouse/reconciliation/config/aws/S3ClientConfig.java
@@ -1,9 +1,8 @@
 package uk.gov.companieshouse.reconciliation.config.aws;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -15,10 +14,8 @@ public class S3ClientConfig {
     private String awsRegion;
 
     @Bean
-    public S3Client s3Client(CamelContext camelContext) {
-        S3Client s3Client = S3Client.builder().region(Region.of(awsRegion))
+    public S3Client s3Client() {
+        return S3Client.builder().region(Region.of(awsRegion))
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create()).build();
-        camelContext.getRegistry().bind("s3Client", s3Client);
-        return s3Client;
     }
 }
