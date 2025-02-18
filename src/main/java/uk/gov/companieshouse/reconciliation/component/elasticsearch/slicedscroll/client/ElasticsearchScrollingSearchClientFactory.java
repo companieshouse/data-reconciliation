81a17bb96c689feb2074f85402c4129aeca48cc2
dhawes-ch <dhawes@companieshouse.gov.uk>
2025-02-18 14:52:48 +0000

    increase elastic client timeout

--- src/main/java/uk/gov/companieshouse/reconciliation/component/elasticsearch/slicedscroll/client/ElasticsearchScrollingSearchClientFactory.java
+++ src/main/java/uk/gov/companieshouse/reconciliation/component/elasticsearch/slicedscroll/client/ElasticsearchScrollingSearchClientFactory.java
@@ -5,16 +5,17 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 
 /**
- * Constructs {@link ElasticsearchScrollingSearchClient clients} used to initiate and continue Elasticsearch
- * sliced scrolling search sessions.
+ * Constructs {@link ElasticsearchScrollingSearchClient clients} used to initiate and continue
+ * Elasticsearch sliced scrolling search sessions.
  */
 public class ElasticsearchScrollingSearchClientFactory {
 
-    public ElasticsearchScrollingSearchClient build(String hostname, int port, String scheme, String index, int size, long timeout, String sliceField) {
+    public ElasticsearchScrollingSearchClient build(String hostname, int port, String scheme,
+            String index, int size, long timeout, String sliceField) {
         return new ElasticsearchScrollingSearchClient(new RestHighLevelClient(
-                RestClient.builder(
-                    new HttpHost(hostname, port, scheme)
-                )
-        ), index, size, timeout, sliceField, new ElasticsearchSlicedScrollValidator());
+                RestClient.builder(new HttpHost(hostname, port, scheme)).setRequestConfigCallback(
+                                builder -> builder.setConnectTimeout(5000).setSocketTimeout(60000))
+                        .setMaxRetryTimeoutMillis(60000)), index, size, timeout, sliceField,
+                new ElasticsearchSlicedScrollValidator());
     }
 }
