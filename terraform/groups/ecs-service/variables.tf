d1e4ca3ba091f7be2ec78efc9aeff1d4f0ffc2bd
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2025-03-14 10:55:14 +0000

    adding var minimum task count

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -50,6 +50,12 @@ variable "max_task_count" {
   default     = 1
 }
 
+variable "min_task_count" {
+  type        = number
+  description = "The minimum number of tasks for this service."
+  default     = 1
+}
+
 variable "use_fargate" {
   type        = bool
   description = "If true, sets the required capabilities for all containers in the task definition to use FARGATE, false uses EC2"
