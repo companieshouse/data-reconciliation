2488eb8cdb6fc6c65980b7e7efb48d07737a1306
makhtar-ch <137518530+makhtar-ch@users.noreply.github.com>
2025-04-23 09:58:33 +0100

    set-read_only_root_file_System-to-true

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -73,7 +73,6 @@ module "ecs-service" {
   use_capacity_provider              = var.use_capacity_provider
   use_fargate                        = var.use_fargate
   fargate_subnets                    = local.application_subnet_ids
-  read_only_root_filesystem          = false
 
   # Scheduler configuration
   enable_eventbridge_scheduler                   = var.enable_eventbridge_scheduler
